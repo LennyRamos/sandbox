f07a301e3d9e221c55464d78e5d4540ace54fc88
LennyRamos <tuf18062@temple.edu>
2019-10-28 21:49:02 -0400

    made toast

--- app/src/main/java/com/kobesoft/sandbox/MainActivity.kt
+++ app/src/main/java/com/kobesoft/sandbox/MainActivity.kt
@@ -6,11 +6,25 @@ package com.kobesoft.sandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rollButton = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Creates a toast on roll dice button click
+     */
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 }
